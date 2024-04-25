87720e247a2f68d16c2bfefba47571e56eb5c72d
Rafal <kolaczekraf@gmail.com>
2024-04-25 12:54:06 +0200

    test

---
+++ src/DependencyInjection/KolrafTestExtension.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Kolraf\TestBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class KolrafTestExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../../config')
+        );
+
+        $loader->load('services.yaml');
+    }
+}
